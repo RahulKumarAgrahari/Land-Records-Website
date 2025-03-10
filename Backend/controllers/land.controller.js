fb5e0234326afa977ea479c177990c38eb22346b
abhishek-fple <abhishek@fpletech.in>
2025-03-11 02:45:46 +0530

    clerch listing done

--- Backend/controllers/land.controller.js
+++ Backend/controllers/land.controller.js
@@ -23,9 +23,9 @@ const registerLand = async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     const body = req.body
     try {
-        const token=req.headers.authrization
+        const token = req.headers.authrization
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const landData = await Land.create({...body, createdBy: decoded.id})
+        const landData = await Land.create({ ...body, createdBy: decoded.id })
         if (landData) {
             await LandHistory.create({
                 landId: landData._id,
@@ -71,25 +71,97 @@ const registerLand = async (req, res) => {
     }
 }
 
-const getLandRecord = async(req, res) => {
+const getLandRecord = async (req, res) => {
     res.header("Access-Control-Allow-Origin", "*");
     const body = req.body
     try {
-        const token=req.headers.authrization
+        const token = req.headers.authrization
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
-        const landData = await Land.find({createdBy:decoded.id}).skip(body.skip).limit(body.limit);
+        const landData = await Land.find({ createdBy: decoded.id }).skip(body.skip).limit(body.limit);
         const totalRecords = await Land.countDocuments({ createdBy: decoded.id });
         console.log(landData)
         if (landData) {
             res.send({
                 message: "land register application submitted sucessfully",
                 status: true,
-                data:landData,
+                data: landData,
                 pagination: {
                     currentPage: body.page,
                     totalPages: Math.ceil(totalRecords / body.limit),
-                    total: totalRecords 
+                    total: totalRecords
                 }
+            })
+        } else {
+            res.status(500).json({
+                message: 'Somthing went wrong',
+                errors: errorMessages,
+                status: false
+            });
+        }
+    } catch (err) {
+
+        if (err.name === 'ValidationError') {
+            // Collect all validation errors
+            const errorMessages = [];
+            for (let field in err.errors) {
+                errorMessages.push(err.errors[field].message); // Store the error messages
+            }
+
+            // Return the errors to the client
+            res.status(400).json({
+                message: 'Validation errors',
+                errors: errorMessages,
+                status: false
+            });
+            return
+        }
+
+        // For other errors (e.g., database issues)
+        res.status(500).json({
+            message: 'Internal Server Error',
+            error: err.message,
+            status: false
+        });
+    }
+}
+const getLandRecordCondition = async (dbData) => {
+    const query = {}
+    Object.keys(dbData).forEach((key) => {
+        if (dbData[key] && !['limit', 'page'].includes(key)) {
+            query[key] = dbData[key]
+        }
+    })
+    const landData = await Land.find(query).skip((dbData.page - 1) * dbData.limit).limit(dbData.limit);
+    const totalRecords = await Land.countDocuments(query);
+    return {
+        data: landData,
+        pagination: {
+            currentPage: dbData.page,
+            totalPages: Math.ceil(totalRecords / dbData.limit),
+            total: totalRecords
+        }
+    }
+}
+const getLandRecordClerk = async (req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    const body = req.body
+    try {
+        const token = req.headers.authrization
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        // const landData = await Land.find({createdBy:decoded.id}).skip(body.skip).limit(body.limit);
+        // const totalRecords = await Land.countDocuments({ createdBy: decoded.id });
+        // console.log(landData)
+        const landData = await getLandRecordCondition({
+            reviewedBy:'',
+            status:'pending',
+            limit:body.limit,
+            page:body.page
+        })
+        if (landData) {
+            res.send({
+                message: "sucess",
+                status: true,
+                ...landData
             })
         } else {
             res.status(500).json({
@@ -126,5 +198,6 @@ const getLandRecord = async(req, res) => {
 }
 export {
     registerLand,
-    getLandRecord
+    getLandRecord,
+    getLandRecordClerk
 }
--- Backend/routes/land.routes.js
+++ Backend/routes/land.routes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { getLandRecord, registerLand } from "../controllers/land.controller.js";
+import { getLandRecord, getLandRecordClerk, registerLand } from "../controllers/land.controller.js";
 
 const router = express.Router();
 
 router.post("/register", registerLand);
 router.post("/list", getLandRecord);
+router.post("/list-clerk", getLandRecordClerk);
 
 export default router;
